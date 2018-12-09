97823af80e4b817e7f736ab7b333be7605c35249
landradeboth <lucas.deandradeboth@stud.tu-darmstadt.de>
2018-12-09 18:02:45 +0100

    Creation

---
+++ Programm/Blink/src/main.cpp
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+  /* 
+  $ Comment 222
+  */
+  pinMode(4,OUTPUT);
+}
+
+void loop() {
+  digitalWrite(4,HIGH);
+  Serial.println("ON"); 
+  delay(500);
+  digitalWrite(4,LOW);
+  Serial.println("OFF");
+  delay(500);
+}
